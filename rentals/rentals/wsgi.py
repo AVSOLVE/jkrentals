675f75dbf5c1be798b1bdd51cf7113a3d57959c0
Alexandre Lima <alexandrelima.mail@gmail.com>
2024-05-16 21:58:11 -0300

    wsgi

--- rentals/rentals/wsgi.py
+++ rentals/rentals/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for rentals project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Add your project directory to the sys.path
+project_home = '/home/alexandrelima/rentals'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Add your virtualenv directory to the sys.path
+venv_path = os.path.join(project_home, 'env', 'lib', 'python3.10', 'site-packages')
+if venv_path not in sys.path:
+    sys.path.append(venv_path)
+
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rentals.settings'
+
+# Import Django's WSGI handler and assign it to `application`
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentals.settings')
-
 application = get_wsgi_application()
